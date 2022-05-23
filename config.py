7907236ff163c59a807d397ca145b4724ad348cb
FlightsAntalya <cheapflights2022@gmail.com>
2022-05-23 22:03:28 +0300

    LatestUpdate

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-BOT_TOKEN = "5344285897:AAETtCyA93hLe3OWFdILilpHY99cW7V6KiU"
-APP_URL = "https://cheap-flights-antalya.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "2123980473:AAH06b3kY8yRBgMKSgCRolykO40XJKbpJO0"
+APP_URL = "https://vitaljadatabase.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
