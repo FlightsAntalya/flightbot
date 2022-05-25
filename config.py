504377a0fc49fba898c63ce606667b92e7e06cd7
FlightsAntalya <cheapflights2022@gmail.com>
2022-05-25 15:18:46 +0300

    nice

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5325836917:AAELctkQVzz7gJllmU0UQYyzvubxq5cDxMM"
-APP_URL = "https://cheap-flights-antalya.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://flight-bot.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://fdclhhzumuymek:9c34d07fca43e2f221b5cf32722e4c418da7fceb936090f447270667a669f07f@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/db0hd61jj9vvpj"
